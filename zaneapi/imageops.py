765214327215c0d93512ebca33a9258d02696894
XuaTheGrate <williamblakelock.wb@gmail.com>
2019-01-13 13:56:31 +1300

    add a few PIL functions

--- zaneapi/imageops.py
+++ zaneapi/imageops.py
@@ -1,8 +1,11 @@
 import io
 import asyncio
 import functools
+from random import shuffle
 
+import colorsys
 from wand.image import Image
+from PIL import Image as PILImage
 
 loop = asyncio.get_event_loop()
 
@@ -12,16 +15,56 @@ def resize(img: Image, width: int):
         aspect = img.width / img.height
         img.sample(width=int(width), height=int(width * aspect))
     return img
+	
+
+def desat(img: PILImage, threshold: int = 2):
+    img_new = PILImage.new("RGBA", img.size)
+    _x, _y = img.size
+    for x in range(_x):
+        for y in range(_y):
+            px = img.getpixel((x, y))
+            h, s, v = colorsys.rgb_to_hsv(*px[:3])
+            s /= threshold
+            img_new.putpixel((x, y), tuple(map(int, colorsys.hsv_to_rgb(h, s, v))))
+    return img_new
+    
+
+def colormap(img: PILImage, color: tuple):
+    bh, _, _ = colorsys.rgb_to_hsv(*color)
+    _x, _y = img.size
+    img_new = PILImage.new("RGBA", img.size)
+    for x in range(_x):
+        for y in range(_y):
+            px = img.getpixel((x, y))
+            _, s, v = colorsys.rgb_to_hsv(*px[:3])
+            r, g, b = colorsys.hsv_to_rgb(bh, s, v)
+            img_new.putpixel((x, y), tuple(map(int, (r, g, b))))
+    return img_new
+
+
+def noise(img: PILImage):
+    _x, _y = img.size
+    img_new = PILImage.new("RGBA", img.size)
+    for x in range(_x):
+        for y in range(_y):
+            px = img.getpixel((x, y))
+            _px = list(px[:3])
+            shuffle(_px)
+            img_new.putpixel((x, y), tuple(_px))
+    return img_new
 
 
 async def image_function(input_img: Image, func, *args):
     executor = functools.partial(func, input_img, *args)
     output_img = await loop.run_in_executor(None, executor)
 
-    assert isinstance(output_img, Image)
+    # assert isinstance(output_img, (Image, PILImage))
 
     b_io = io.BytesIO()
-    output_img.save(b_io)
+    if isinstance(output_img, Image):
+        output_img.save(b_io)
+    else:
+        output_img.save(b_io, "png")
     b_io.seek(0)
 
     return b_io
