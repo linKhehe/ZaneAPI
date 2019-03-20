fe69d42482842bb31e003de1a0369819ed0ea2bc
Liam H <35095843+ir-3@users.noreply.github.com>
2019-03-20 14:24:19 -0500

    Make the API blueprint use the new imageops

--- zaneapi/blueprints/api.py
+++ zaneapi/blueprints/api.py
@@ -1,12 +1,14 @@
-from quart import Blueprint, request, jsonify, make_response
-from wand.color import Color
-from skimage import io
-import skimage
-
-from ..imageops import *
+import ..imageops
+import io
 
 bp = Blueprint('api', __name__)
 
+
+async def add_headers(response, content_type: str, status: int):
+    response.headers['Content-Type'] = content_type
+    response.headers['Status'] = status
+
+    return response
 
 @bp.route("/")
 @bp.route("/status")
@@ -19,366 +21,145 @@ async def status():
              }
         )
     )
-    response.headers['Content-Type'] = "application/json"
-    response.headers['Status'] = 200
-
-    return response
+    return await add_headers(response, "application/json", 200)
 
 
 @bp.route("/desat", methods=["POST"])
 @bp.route("/desaturation", methods=["POST"])
 async def desat_endpoint():
     threshold = int(request.args.get("threshold") or 2)
-    image = Image(blob=await request.body)
-    image = await image_function(image, desat, threshold)
 
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.desat(io.BytesIO(await request.body), threshold).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/sat", methods=["POST"])
 @bp.route("/saturation", methods=["POST"])
 async def sat_endpoint():
     threshold = int(request.args.get("threshold") or 1)
-    image = Image(blob=await request.body)
-    image = await image_function(image, sat, threshold)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
-
-
-@bp.route("/colormap", methods=["POST"])
-async def colormap_endpoint():
-    color_arg = f'#{((request.args.get("color") or "7289DA").replace("0x", "#").lstrip("#")):0>6}'
-    color_obj = Color(color_arg)
-    rgb = color_obj.red_int8, color_obj.green_int8, color_obj.blue_int8
-    image = PILImage.open(io.BytesIO(await request.body))
-    transformed = await image_function(image, colormap, rgb)
-
-    assert isinstance(transformed, io.BytesIO)
-
-    response = await make_response(transformed.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    
+    response = await make_response(await imageops.sat(io.BytesIO(await request.body), threshold).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/noise", methods=["POST"])
 async def noise_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, noise)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.noise(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/magic", methods=["POST"])
 @bp.route("/magik", methods=["POST"])
 async def magic_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, magic)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.magic(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/deepfry", methods=["POST"])
 async def deepfry_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, deepfry)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.deepfry(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/invert", methods=["POST"])
 @bp.route("/negate", methods=["POST"])
 async def invert_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, invert)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.invert(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/arc", methods=["POST"])
 async def arc_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, arc)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.arc(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/concave", methods=["POST"])
 async def concave_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, concave)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.concave(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/convex", methods=["POST"])
 async def convex_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, convex)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.convex(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/floor", methods=["POST"])
 async def floor_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, floor)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.floor(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/blur", methods=["POST"])
 async def blur_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, blur)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.blur(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/vaporwave", methods=["POST"])
 async def vaporwave_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, vaporwave)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.vaporwave(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/emboss", methods=["POST"])
 async def emboss_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, emboss)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.emboss(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/shade", methods=["POST"])
+@bp.route("/sobel", methods=["POST"])
 async def shade_endpoint():
-    image = PILImage.open(io.BytesIO(await request.body))
-    image = await image_function(image, shade)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.sobel(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/edge", methods=["POST"])
 async def edge_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, edge)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.edge(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/bend", methods=["POST"])
 async def bend_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, bend)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.bend(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/posterize", methods=["POST"])
 async def posterize_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, posterize)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.posterize(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/grayscale", methods=["POST"])
 async def grayscale_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, grayscale)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.greyscale(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/lsd", methods=["POST"])
 async def lsd_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, lsd)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.lsd(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/sort", methods=["POST"])
 async def sort_endpoint():
-    image = skimage.io.imread(io.BytesIO(await request.body))
-    image = await image_function(image, sort)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
-
-
-# @bp.route("/histogram", methods=["POST"])
-# async def histogram_endpoint():
-#     image = skimage.io.imread(io.BytesIO(await request.body))
-#     image = await image_function(image, histogram)
-#
-#     assert isinstance(image, io.BytesIO)
-#
-#     response = await make_response(image.getvalue())
-#     response.headers['Status'] = 200
-#     response.headers['Content-Type'] = "image/png"
-#
-#     return response
+    response = await make_response(await imageops.sort(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/gay", methods=["POST"])
 async def gay_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, gay)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
-
-
-@bp.route("/straight", methods=["POST"])
-async def straight_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, straight)
-
-    assert isinstance(image, io.BytesIO)
-
-    response = await make_response(image.getvalue())
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "image/png"
-
-    return response
+    response = await make_response(await imageops.gay(io.BytesIO(await request.body)).getvalue())
+    return await add_headers(response, "image/png", 200)
 
 
 @bp.route("/ascii", methods=["POST"])
 async def ascii_endpoint():
-    image = Image(blob=await request.body)
-    image = await image_function(image, ascii_art)
-
-    assert isinstance(image, str)
-
-    response = await make_response(jsonify({"ascii": image}))
-    response.headers['Status'] = 200
-    response.headers['Content-Type'] = "application/json"
-
-    return response
+    response = await make_response(jsonify({"ascii": await imageops.ascii(io.BytesIO(await request.body))}))
+    return await add_headers(response, "application/json", 200)
